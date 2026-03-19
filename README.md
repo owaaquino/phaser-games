98772e94eed87bbbb8e93c7fc3743392a6c3a487
owaaquino <josejoshuaaquino@gmail.com>
2026-03-19 17:32:17 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# pahser-games
---
+++ game_1_astroman/assets/tilesets/runner-asset-sheet.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="runner-asset-sheet-with-transparency" tilewidth="32" tileheight="32" tilecount="240" columns="16">
+ <image source="../../../../../../../Downloads/Space Runner Assets - 27-11-20/Space Runner Assets - 27-11-20/runner-asset-sheet-with-transparency.png" width="512" height="480"/>
+ <tile id="153">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="154">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="155">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="156">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="169">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
