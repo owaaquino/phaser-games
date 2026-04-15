c957bba3399c653c3ff155633caf1ee09c6cfd2b
owaaquino <josejoshuaaquino@gmail.com>
2026-04-15 15:09:17 +0800

    Add Player class and implement player creation and animations

--- game_1_astroman/src/scenes/GameScene.js
+++ game_1_astroman/src/scenes/GameScene.js
@@ -1,21 +1,45 @@
+import { Player } from '../components/player.js';
+
 class GameScene extends Phaser.Scene {
   constructor() {
     super('GameScene');
   }
   create() {
-    // Create menu elements
+    // Create level
     const map = this.make.tilemap({ key: 'level5' });
     const tileset = map.addTilesetImage('runner-asset-sheet', 'tileimage');
 
-    const background1 = map.createLayer('background1', tileset, 0, 0);
-    const background2 = map.createLayer('background2', tileset, 0, 0);
+    map.createLayer('background1', tileset, 0, 0);
+    map.createLayer('background2', tileset, 0, 0);
 
     const movingplatforms = map.createLayer('moving-platform', tileset, 0, 0);
 
     const frame = map.createLayer('framebound', tileset, 0, 0);
     const platforms = map.createLayer('platforms', tileset, 0, 0);
+    const startingPoint = map.findObject(
+      'player_starting_point',
+      (obj) => true,
+    );
 
-    platforms.createLevel;
+    // Create input keys
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    // Create player
+    const playerInstance = new Player(this, startingPoint);
+    playerInstance.createPlayer();
+    this.player = playerInstance.player; // Store sprite on scene
+    this.playerController = playerInstance; // Store controller for updates
+
+    // Object layers collisions
+
+    frame.setCollisionByProperty({ collides: true });
+    platforms.setCollisionByProperty({ collides: true });
+    this.physics.add.collider(this.player, frame);
+    this.physics.add.collider(this.player, platforms);
+  }
+
+  update() {
+    this.playerController.update(this.cursors);
   }
 }
 
