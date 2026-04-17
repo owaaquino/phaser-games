cbd9a6795123f5f8e4597862a0522f55bb353d42
owaaquino <josejoshuaaquino@gmail.com>
2026-04-18 05:28:02 +0800

    Add Enemies class and integrate enemy creation and behavior into GameScene

---
+++ game_1_astroman/src/components/enemies.js
@@ -0,0 +1,63 @@
+export class Enemies {
+  constructor(scene) {
+    this.scene = scene;
+  }
+
+  createEnemies(map) {
+    this.scene.anims.create({
+      key: 'alien-walk',
+      frames: this.scene.anims.generateFrameNumbers('alien-run', {
+        start: 0,
+        end: 3,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.scene.anims.create({
+      key: 'alien-idle',
+      frames: this.scene.anims.generateFrameNumbers('alien-idle', {
+        start: 0,
+        end: 3,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    const enemyObjects = map.getObjectLayer('aliens')['objects'];
+    this.enemyObjects = this.scene.physics.add.group();
+
+    const enemyLimits = map.getObjectLayer('enemy_limits')['objects'];
+    this.enemyLimits = this.scene.physics.add.group();
+
+    enemyObjects.forEach((object) => {
+      let obj = this.enemyObjects.create(object.x, object.y, 'alien-idle');
+      obj.setSize(24, 24);
+      obj.setOffset(4, 2);
+    });
+
+    enemyLimits.forEach((object) => {
+      const wall = this.enemyLimits.create(object.x, object.y, null);
+      wall.setOrigin(0, 0); // Set origin to top-left corner
+      wall.setSize(object.width, object.height); // Set the size of the wall to match the object
+      wall.setVisible(false); // Hide the wall
+
+      wall.body.immovable = true; // Make the wall immovable
+    });
+  }
+
+  update() {
+    this.enemyObjects.children.iterate(function (child) {
+      if (child.body.velocity.x === 0) {
+        const direction = Phaser.Math.RND.pick([-50, 50]); // randomly pick left or right
+        child.setVelocityX(direction);
+        child.anims.play('alien-walk', true);
+        if (child.body.velocity.x < 0) {
+          child.flipX = true;
+        } else {
+          child.flipX = false;
+        }
+      }
+    });
+  }
+}
