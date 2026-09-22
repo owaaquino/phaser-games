96ed700984f32c2ab5aa62d0d72a6bc988c243db
owaaquino <josejoshuaaquino@gmail.com>
2026-09-22 11:16:17 +0800

    FIX: remove unnecessary score change emissions in GameManager methods

--- game_2_fantasyman/src/GameManager.js
+++ game_2_fantasyman/src/GameManager.js
@@ -16,12 +16,10 @@ class GameManager extends Phaser.Events.EventEmitter {
 
   updateTotalKills() {
     this.state.totalEnemyKilled += 1;
-    this.emit('KILLS_SCORE_CHANGED', this.state.totalEnemyKilled);
   }
 
   updateTotalRetry() {
     this.state.totalRetry += 1;
-    this.emit('RETRY_SCORE_CHANGED', this.state.totalRetry);
   }
 
   resetGame() {
@@ -29,8 +27,6 @@ class GameManager extends Phaser.Events.EventEmitter {
     this.state.totalEnemyKilled = 0;
     this.state.totalRetry = 0;
     this.state.isDead = false;
-    this.emit('RETRY_SCORE_CHANGED', 0);
-    this.emit('KILLS_SCORE_CHNAGED', 0);
 
     this.removeAllListeners();
   }
